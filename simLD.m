d9229d5eb6dabc35420c81be191b2e7465788fb8
Eurico Pedrosa <efp@ua.pt>
2009-03-27 15:39:19 +0000

    work in progress

--- simLD.m
+++ simLD.m
@@ -11,8 +11,10 @@
 % 
 % @parametros
 %
-%     TCP : Taxa de Chegada de Pacotes ( mu )
-%     TMP : Tamanho Medio do Pacote ( bytes )
+%     TCP : Taxa de Chegada de Pacotes ( lambda )
+%     TCPV: Taxa de Cegada de Pacotes VoIP
+%     TMP : Tamanho Medio do Pacote ( em bytes )
+%     TMPV: Tamanho Medio do Pacote VoIP ( em bytes )
 %     CL  : Capacidade da Ligacao ( em Mbps )
 %     TFE : Tamanho da Fila de Espera ( em bytes )
 %     NP  : Numero de pacotes em que se baseia o criterio de paragem
@@ -24,17 +26,21 @@
 %     AMaxP : Atraso maximo de pacotes
 %     OMF   : Ocupacao media da fila de espera
 %%
-function [TPD, AMP, AMaxP, OMF] = simLD( TCP, TMP, CL, TFE, NP )
+function [TPD, AMP, AMaxP, OMF] = simLD( TCP, TCPV, TMP, TMPV, CL, TFE, NP )
 
 % Constantes do sistema
 
 SISTEMA_LIVRE   = 0;
 SISTEMA_OCUPADO = 1;
 
+TYPE_DATA = 0;
+TYPE_VOIP = 1;
+
 % Variaveis do sistema
 CapacidadeDaLigacao = ( CL * 1000 * 1000 ) / 8;   % Capacidade da ligação em bytes por segundo 
 TamanhoDaFilaDeEspera = TFE;                      % Tamanho da fila de espera em bytes
 TempoMedioChegadaPacotes = 1 / TCP;
+TempoMedioChegadaPacotesVoIP = 1 / TCPV;
 
 Estado = SISTEMA_LIVRE;
 
@@ -56,38 +62,64 @@ FilaDeEspera = [ ];
 % -- variaveis temporais -- %
 Tempo = 0;
 
-Chegada = Tempo + exprnd( 1 / TCP );
-Partida = Inf;
+% Gerar chegadas aleatorias das chamadas de ambas as classes
+VoIPtime = rnd * (1.5*TempoMedioChegadaPacotesVoIP - 0.5*TempoMedioChegadaPacotesVoIP) +
+           ( 0.5* TempoMedioChegadaPacotesVoIP );
+
+Chegada = [ [exprnd( TempoMedioChegadaPacotes ), TYPE_DATA ];
+            [VoIPtime, TYPE_VOIP] ];
+% Ordenar as chegadas das chamadas por ordem crescente
+[s, i] = sort( Chegada(:,1) );
+Chegada = Chegada(i,:);
+
+Partida = [Inf , TYPE_DATA] ;
 
 
 % -- Ciclo da Simulacao -- %
 while ( 1 ),
 
-  if ( Chegada < Partida )
+  if ( Chegada(1,1) < Partida(1,1) )
     % -- Temos uma chegada -- %
     TempoUltimoInstante = Tempo;
     Tempo = Chegada;
     %TotalFila =
     IOcupacao = IOcupacao + OcupacaoFila * (Tempo - TempoUltimoInstante);
-    do,
-        TamanhoPacote = round( exprnd( TMP ) );  % Tamanho do pacote em bytes
-    until ( TamanhoPacote > 48 && TamanhoPacote < 1500 );
     
-    TotalPacotes = TotalPacotes + 1;
-    Chegada = Tempo + exprnd( TempoMedioChegadaPacotes );   % Agendar próxima chegada 
-    
-    if ( Estado == SISTEMA_LIVRE )
-      Estado = SISTEMA_OCUPADO;
-      Instante = Tempo;
-      Partida  = Tempo + ( TamanhoPacote / CapacidadeDaLigacao );
-    else
-      if ( (TamanhoPacote + OcupacaoFila) > TamanhoDaFilaDeEspera  )
-        PacotesPerdidos = PacotesPerdidos + 1;
+    if ( Chegada(1,2) == TYPE_DATA )
+      % -- Pacote do tipo de dados -- %
+      do,
+      TamanhoPacote = round( exprnd( TMP ) );  % Tamanho do pacote em bytes
+      until ( TamanhoPacote > 48 && TamanhoPacote < 1500 );
+
+      TotalPacotes = TotalPacotes + 1
+      ;
+      % Calcula a proxima chegada de chamada
+      ProximaChegada = Tempo + exprnd( TMP );
+      % Remover a chamada actual e adicionar a proxima chamada ordenadamente
+      Chegada = [ Chegada(2:end,:) ; [ ProximaChegada, TYPE_DATA] ];
+      [s, i] = sort( Chegada(:,1) );
+      Chegada = Chegada(i,:);
+
+      if ( Estado == SISTEMA_LIVRE )
+        Estado = SISTEMA_OCUPADO;
+        Instante = Tempo;
+        ProximaPartida  = Tempo + ( TamanhoPacote / CapacidadeDaLigacao );
+        % Adiciona ordenadamente o fim da chamada
+        Partida = [ Partida ; [ ProximaPartida, CLASS_A ] ];
+        [s, i] = sort( Partida(:,1) );
+        Partida = Partida(i,:);
       else
-        FilaDeEspera = [ FilaDeEspera ; [ Tempo, TamanhoPacote ]  ];
-        OcupacaoFila = OcupacaoFila + TamanhoPacote;
+        if ( (TamanhoPacote + OcupacaoFila) > TamanhoDaFilaDeEspera  )
+          PacotesPerdidos = PacotesPerdidos + 1;
+        else
+          FilaDeEspera = [ FilaDeEspera ; [ Tempo, TamanhoPacote ]  ];
+          OcupacaoFila = OcupacaoFila + TamanhoPacote;
+        end;
       end;
-    end;
+      
+    else % if ( chegada == TYPE_DATA )
+      % -- Pacote do tipo VoIP -- %
+    end; %end if ( chegada == TYPE_DATA )
     
   else % else ( Chegada < Partida )
     % -- Temos uma Partida -- %
