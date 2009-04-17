c5e1c86ce71505d6afaa164f9ba779482dfaea27
VuDu <vudu.curse@gmail.com>
2009-04-17 13:05:00 +0100

    first version of tp2_e

---
+++ tp2_e.m
@@ -0,0 +1,123 @@
+
+%   pt.ua.deti.ddr.tp2
+% 
+%   DDR - Trabalho pratico #2
+%
+%   Simulador de Ligacao de Dados.
+%
+%
+
+%%
+% 
+% @parametros
+%
+%     TCPD : Taxa de Chegada de Pacotes de Dados ( lambda )
+%     TCPV : Taxa de Chegada de Pacotes VoIP ( lambda )
+%     TMPD : Tamanho Medio do Pacote de Dados ( bytes )
+%     TMPV : Tamanho Medio do Pacote VoIP ( bytes )
+%     TFE : Tamanho da Fila de Espera ( bytes )
+%     REP : Número de repetições da simulação
+%
+%     CL  : Capacidade da Ligacao ( Mbps )
+%     NP  : Numero de pacotes em que se baseia o criterio de paragem
+%
+% @saida
+%
+%     TPD   : Taxa de perda de pacotes
+%     AMP   : Atraso medio de pacotes ( ms )
+%     AMaxP : Atraso maximo de pacotes ( ms )
+%     OMF   : Ocupacao media da fila de espera ( bytes )
+%%
+
+function tp2_e( TCPD, TCPV, TMPD, TMPV, TFE, Rep )
+
+TMP = 600;
+CL  = 2;
+%TFE = 1000000;
+NP  = 1000;
+
+TPD   = 1 : Rep;
+AMP   = 1 : Rep;
+AMaxP = 1 : Rep;
+OMF   = 1 : Rep;
+
+for i = 1:Rep,
+  [ TPD(i), TPDDados(i), TPDVoIP(i), AMP(i), AMPDados(i), AMPVoIP(i), AMaxP(i), AMaxPDados(i), AMaxPVoIP(i), OMF(i) ] = simLD( TCPD, TCPV, TMPD, TMPV, CL, TFE, NP );
+end
+
+AMP   = AMP * 1000;      % seg -> ms
+AMaxP = AMaxP * 1000;    % seg -> ms
+
+AMPDados   = AMPDados * 1000;      % seg -> ms
+AMaxPDados = AMaxPDados * 1000;    % seg -> ms
+
+AMPVoIP   = AMPVoIP * 1000;      % seg -> ms
+AMaxPVoIP = AMaxPVoIP * 1000;    % seg -> ms
+
+
+%     TPD   : Taxa de perda de pacotes
+TPDMean   = mean( TPD )
+TPDVar    = var( TPD )
+z =  norminv(0.95) * sqrt(TPDVar / Rep);
+IntTPD = [ TPDMean - z , TPDMean + z ]
+
+TPDMeanDados   = mean( TPDDados )
+TPDVarDados    = var( TPDDados )
+z =  norminv(0.95) * sqrt(TPDVarDados / Rep);
+IntTPDDados = [ TPDMeanDados - z , TPDMeanDados + z ]
+
+TPDMeanVoIP   = mean( TPDVoIP )
+TPDVarVoIP    = var( TPDVoIP )
+z =  norminv(0.95) * sqrt(TPDVarVoIP / Rep);
+IntTPDVoIP = [ TPDMeanVoIP - z , TPDMeanVoIP + z ]
+
+
+%     AMP   : Atraso medio de pacotes
+AMPMean   = mean( AMP )
+AMPVar    = var( AMP )
+z =  norminv(0.95) * sqrt(AMPVar / Rep);
+IntAMP = [ AMPMean - z , AMPMean + z ]
+
+AMPMeanDados   = mean( AMPDados )
+AMPVarDados    = var( AMPDados )
+z =  norminv(0.95) * sqrt(AMPVarDados / Rep);
+IntAMPDados = [ AMPMeanDados - z , AMPMeanDados + z ]
+
+AMPMeanVoIP   = mean( AMPVoIP )
+AMPVarVoIP    = var( AMPVoIP )
+z =  norminv(0.95) * sqrt(AMPVarVoIP / Rep);
+IntAMPVoIP = [ AMPMeanVoIP - z , AMPMeanVoIP + z ]
+
+
+%     AMaxP : Atraso maximo de pacotes
+AMaxPMean = mean( AMaxP )
+AMaxPVar  = var( AMaxP )
+z =  norminv(0.95) * sqrt(AMaxPVar / Rep);
+IntAMax = [ AMaxPMean - z , AMaxPMean + z ]
+
+AMaxPMeanDados = mean( AMaxPDados )
+AMaxPVarDados  = var( AMaxPDados )
+z =  norminv(0.95) * sqrt(AMaxPVarDados / Rep);
+IntAMaxDados = [ AMaxPMeanDados - z , AMaxPMeanDados + z ]
+
+AMaxPMeanVoIP = mean( AMaxPVoIP )
+AMaxPVarVoIP  = var( AMaxPVoIP )
+z =  norminv(0.95) * sqrt(AMaxPVarVoIP / Rep);
+IntAMaxVoIP = [ AMaxPMeanVoIP - z , AMaxPMeanVoIP + z ]
+
+%     OMF   : Ocupacao media da fila de espera
+OMFMean   = mean( OMF )
+OMFVar    = var( OMF )
+z =  norminv(0.95) * sqrt(OMFVar / Rep);
+IntOMF = [ OMFMean - z , OMFMean + z ]
+
+OMFMeanDados   = mean( OMFDados )
+OMFVarDados    = var( OMFDados )
+z =  norminv(0.95) * sqrt(OMFVarDados / Rep);
+IntOMFDados = [ OMFMeanDados - z , OMFMeanDados + z ]
+
+OMFMeanVoIP   = mean( OMFVoIP )
+OMFVarVoIP    = var( OMFVoIP )
+z =  norminv(0.95) * sqrt(OMFVarVoIP / RepVoIP);
+IntOMFVoIP = [ OMFMeanVoIP - z , OMFMeanVoIP + z ]
+
