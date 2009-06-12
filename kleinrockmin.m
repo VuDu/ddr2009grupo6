27e7835799b14f85d9a7dd3d52f211a9093b1601
Eurico Pedrosa <efp@ua.pt>
2009-06-13 00:40:50 +0100

    teorico do custo minimo

---
+++ kleinrockmin.m
@@ -0,0 +1,57 @@
+%
+%   pt.ua.deti.ddr.tp2
+% 
+%   DDR - Trabalho pratico #3
+%
+%   Simulador de uma Rede de Comutação de Pacotes.
+%
+
+%%
+%
+% @saida
+%
+%     Ws     : Atraso médio no fluxo s (mseg)
+%
+%%
+
+function [ Ws ] = kleinrock( )
+
+
+TamMedPacote = 600;  % bytes
+CapacidadeLigacao = 10000000;  % bits/seg
+
+%  lambda
+%lambda = [ 400, 400, 400, 400, 800, 800, 1000, 1000 ];  % pacotes/seg
+
+%lambda = [ 800, 800, 1400, 1400, 400, 400, 400, 400,  ];  % pacotes/seg
+
+lambda(1,2) = 400 + 400;
+lambda(2,1) = 400 + 400;
+
+lambda(2,3) = 1000 + 400;
+lambda(3,2) = 1000 + 400;
+
+lambda(3,5) = 400;
+lambda(5,3) = 400;
+
+lambda(1,4) = 400;
+lambda(4,1) = 400;
+ 
+%  mu
+mu = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
+for i = 1 : 8,
+	mu(i) = ((10000000 / 8) / TamMedPacote);  % pacotes/segundo
+end;
+
+% Fluxo 1
+Ws(1) = ( 1/( mu(1) - lambda(1,2) ) )*1000;
+Ws(2) = ( 1/( mu(2) - lambda(2,1) ) )*1000; 
+Ws(3) = ( 1/( mu(3) - lambda(1,4) ) )*1000; 
+Ws(4) = ( 1/( mu(4) - lambda(4,1) ) )*1000;
+Ws(5) = ( 1/( mu(1) - lambda(1,2) ) + 1/( mu(1) - lambda(2,3) ) + 1/( mu(1) - lambda(3,5) ) )*1000; 
+Ws(6) = ( 1/( mu(1) - lambda(2,1) ) + 1/( mu(1) - lambda(3,2) ) + 1/( mu(1) - lambda(3,5) ) )*1000; 
+Ws(7) = ( 1/( mu(7) - lambda(2,3) ) )* 1000;
+Ws(8) = ( 1/( mu(8) - lambda(3,2) ) )* 1000;
+
+
+
