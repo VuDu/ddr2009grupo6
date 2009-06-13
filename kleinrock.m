73f6aa2d7c5c4f502202bd9d71a4b98f18900f40
Eurico Pedrosa <efp@ua.pt>
2009-06-13 01:44:45 +0100

    renames

--- kleinrock.m
+++ kleinrock.m
@@ -20,20 +20,18 @@ function [ Ws ] = kleinrock( )
 TamMedPacote = 600;  % bytes
 CapacidadeLigacao = 10000000;  % bits/seg
 
-%  lambda
+lambda(1,2) = 400 + 400;
+lambda(2,1) = 400 + 400;
 
-lambda(1,2) = 400;
-lambda(2,1) = 400;
+lambda(2,3) = 1000 + 400;
+lambda(3,2) = 1000 + 400;
 
-lambda(2,3) = 1000;
-lambda(3,2) = 1000;
+lambda(3,5) = 400;
+lambda(5,3) = 400;
 
-lambda(1,4) = 800;
-lambda(4,1) = 800;
-
-lambda(4,5) = 400;
-lambda(5,4) = 400;
-
+lambda(1,4) = 400;
+lambda(4,1) = 400;
+ 
 %  mu
 mu = [ 0, 0, 0, 0, 0, 0, 0, 0 ];
 for i = 1 : 8,
@@ -45,8 +43,8 @@ Ws(1) = ( 1/( mu(1) - lambda(1,2) ) )*1000;
 Ws(2) = ( 1/( mu(2) - lambda(2,1) ) )*1000; 
 Ws(3) = ( 1/( mu(3) - lambda(1,4) ) )*1000; 
 Ws(4) = ( 1/( mu(4) - lambda(4,1) ) )*1000;
-Ws(5) = ( 1/( mu(1) - lambda(1,4) ) + 1/( mu(5) - lambda(4,5) ) )*1000; 
-Ws(6) = ( 1/( mu(6) - lambda(4,1) ) + 1/( mu(2) - lambda(5,4) ) )*1000; 
+Ws(5) = ( 1/( mu(1) - lambda(1,2) ) + 1/( mu(1) - lambda(2,3) ) + 1/( mu(1) - lambda(3,5) ) )*1000; 
+Ws(6) = ( 1/( mu(1) - lambda(2,1) ) + 1/( mu(1) - lambda(3,2) ) + 1/( mu(1) - lambda(3,5) ) )*1000; 
 Ws(7) = ( 1/( mu(7) - lambda(2,3) ) )* 1000;
 Ws(8) = ( 1/( mu(8) - lambda(3,2) ) )* 1000;
 
